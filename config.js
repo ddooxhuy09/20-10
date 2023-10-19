48f89ce38883c793f1bd076fa2ca1c2fb8a4ba30
Do Minh Bao Huy <137915965+ddooxhuy09@users.noreply.github.com>
2023-10-20 00:18:31 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Em';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng em';
+const contentCard = 'Chúc em 20/10 tràn ngập niềm vui và những nụ cười.';
+const giftImage = '1.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
